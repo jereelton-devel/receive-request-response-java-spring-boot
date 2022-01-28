0284b1b5d1595a7676bdc57018c2eeb4402ba750
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-01-28 11:43:03 -0300

    Initial Commit

---
+++ src/test/java/com/receiverequestresponse/app/ReceiveRequestResponseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receiverequestresponse.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiveRequestResponseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
